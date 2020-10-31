c8882b6c6ac05f01f355c71b197979e8c6bda2b9
jonathan-peel <jonathan.n.peel@gmail.com>
2020-10-31 11:35:00 +0100

    Added container display line into default.sh. Code now doesn't terminate automatically (!?)

--- launchers/default.sh
+++ launchers/default.sh
@@ -12,6 +12,10 @@ dt-launchfile-init
 # NOTE: Use the variable DT_REPO_PATH to know the absolute path to your code
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
+# Run below line inside container before running the wrapper to give your container a display
+dt-exec Xvfb :1 -screen 0 1024x768x24 -ac +extension GLX +render -noreset 
+export DISPLAY=:1
+
 # launching app
 dt-exec echo "This is an empty launch script. Update it to launch your application."
 
