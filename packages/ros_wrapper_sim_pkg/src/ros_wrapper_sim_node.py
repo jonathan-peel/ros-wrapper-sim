8e3d1f02ce794473f4b0a89fafb214b106953d10
jonathan-peel <jonathan.n.peel@gmail.com>
2020-11-02 22:53:26 +0100

    Subscribed to wheels_cmd topic

--- packages/ros_wrapper_sim_pkg/src/ros_wrapper_sim_node.py
+++ packages/ros_wrapper_sim_pkg/src/ros_wrapper_sim_node.py
@@ -4,16 +4,27 @@ import os
 import rospy
 from duckietown.dtros import DTROS, NodeType
 from std_msgs.msg import String
+from duckietown_msgs.msg import WheelsCmdStamped
+
 
 class RosWrapperSimNode(DTROS):
 
     def __init__(self, node_name):
         # initialize the DTROS parent class
         super(RosWrapperSimNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
+
+        # get vehicle name
+        vehicle_name = os.environ.get('VEHICLE_NAME', 'default_vehicle')
+        rospy.loginfo("Vehicle name is %s" % vehicle_name)
+
         # construct publisher as a object contained in self
         self.pub = rospy.Publisher('chatter', String, queue_size=10)
+        
         # construct subscriber
-        self.sub = rospy.Subscriber('chatter', String, self.callback)
+        wheels_command_topic = "/%s/wheels_driver_node/wheels_cmd" % vehicle_name
+        self.wheels_command_sub = rospy.Subscriber(wheels_command_topic, WheelsCmdStamped, self.wheel_commands_callback)
+        rospy.loginfo("Subscribing to %s" % wheels_command_topic)
+
 
     def run(self):
         # publish message every 1 second
@@ -24,7 +35,8 @@ class RosWrapperSimNode(DTROS):
             self.pub.publish(message)
             rate.sleep()
 
-    def callback(self, data):
+    def wheel_commands_callback(self, data):
+        # function gets called when sub receives a message, data
         rospy.loginfo("Received message %s", data.data)
 
 if __name__ == '__main__':
